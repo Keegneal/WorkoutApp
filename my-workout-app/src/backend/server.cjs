5a1648c056e7919522059fdaea97108f2f5c21da
Keegneal <92490783+Keegneal@users.noreply.github.com>
2025-01-09 22:20:15 -0600

    connected DB to code

--- my-workout-app/src/backend/server.cjs
+++ my-workout-app/src/backend/server.cjs
@@ -1,10 +1,14 @@
 const express = require('express');
-const mysql= require('mysql');
+const mysql= require('mysql2');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
-const port = 5173;
+app.use(cors());
+app.use(express.json());
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -24,5 +28,16 @@ const db = mysql.createPool({
 
 app.listen(port, () =>{
     console.log(`Server running on port ${port}`);
+})
+
+app.get('/my_workout_app',(req,res)=> {
+
+  db.query('SELECT 1+1 AS solution', (err, results) =>{
+    if(err){
+      return res.status(500).json({error:err});
+    }
+    res.json({result: results[0].solution})
+  })
+
 })
 
